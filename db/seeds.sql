bb8896af3ee42b11b85cc53068b4f4e21da02ae3
annacrespo12 <annacrespo12@gmail.com>
2017-09-28 19:50:49 -0700

    index handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('black bean', true, 07-01-17);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('quinoa', true, 07-01-17);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('portabella mushroom', true, 07-01-17);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('black bean', true, 07-01-17);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('black bean', true, 07-01-17);
+
+SELECT * FROM burgers;
