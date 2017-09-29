fd1ffd6d360259320271d1daabee3267fe41bd66
annacrespo12 <annacrespo12@gmail.com>
2017-09-28 21:12:05 -0700

    buttons

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
@@ -18,9 +18,9 @@ router.get("/", function(req, res) {
 
 router.post("/", function(req, res) {
   burger.create([
-    "name", "devoured"
+    "burger_name, devoured"
   ], [
-    req.body.name, req.body.devoured
+    req.body.burger_name, req.body.devoured
   ], function() {
     res.redirect("/");
   });
@@ -38,13 +38,6 @@ router.put("/:id", function(req, res) {
   });
 });
 
-router.delete("/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function() {
-    res.redirect("/");
-  });
-});
 
 // Export routes for server.js to use.
 module.exports = router;
